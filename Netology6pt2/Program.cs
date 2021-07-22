4b468947329f7d496f5a8947a03a988d58d4a432
Bekhruz Gab <brozermanik@gmail.com>
2021-07-22 13:02:14 +0300

    Reflections

---
+++ Netology6pt2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+
+namespace Netology6pt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cl = new TestClass();
+            var type = cl.GetType();
+            type = typeof(TestClass);
+            
+            var fields = type.GetFields();
+            //8:57
+            var field = type.GetField("FieldInt", BindingFlags.NonPublic | BindingFlags.Instance);
+            field.SetValue(cl, (short)12345);
+
+            Console.WriteLine(field.GetValue(cl));
+            
+            var someProperties = type.GetProperties(BindingFlags.NonPublic | BindingFlags.Instance);
+            var someProperty = type.GetProperty("PropertyString", BindingFlags.NonPublic | BindingFlags.Instance);
+            someProperty.SetValue(cl, "new");
+            
+            Console.WriteLine(someProperty.GetValue(cl));
+        }
+    }
+
+    public class TestClass
+    {
+        private int FieldInt;
+        protected short FieldShort;
+        public long FieldLong;
+        
+        private char PropertyChar { get; set; }
+        protected string PropertyString { get; set; }
+        public DateTime PropertyDateTime { get; set; }
+        
+        private void SetInt(int i) {}
+        protected object GetObject() => null;
+        public TimeSpan GetTimeSpan() => TimeSpan.MaxValue;
+    }
+    
+
+}
